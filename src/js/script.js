51e9fcd1d70969a8b1cf77838cd4cb47560417f9
Samuel Reutcky <sreutcky@gmail.com>
2025-05-09 12:38:01 -0500

    restart

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,17 @@
-'use strict';
-const hamburger = document.querySelector('.hamburger');
-const navUl = document.querySelector('nav ul');
+"use strict";
 
-hamburger.addEventListener('click', () => {
-  navUl.classList.toggle('active');
-});
+function setTextAnimation(delay, duration, strokeWidth, timingFunction, strokeColor,repeat) {
+            let paths = document.querySelectorAll("path");
+            let mode=repeat?'infinite':'forwards'
+            for (let i = 0; i < paths.length; i++) {
+                const path = paths[i];
+                const length = path.getTotalLength();
+                path.style["stroke-dashoffset"] = `${length}px`;
+                path.style["stroke-dasharray"] = `${length}px`;
+                path.style["stroke-width"] = `${strokeWidth}px`;
+                path.style["stroke"] = `${strokeColor}`;
+                path.style["animation"] = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
+                path.style["animation-delay"] = `${i * delay}s`;
+            }
+        }
+setTextAnimation(0.5,8.5,1.5,'linear','#ffffff',false);
